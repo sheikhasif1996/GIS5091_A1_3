6c06ca90e6275f2587d16f74ef91d14661261e5e
Sk Muhammad Asif <91278606+sheikhasif1996@users.noreply.github.com>
2024-01-24 19:13:18 -0600

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ var Stamen_Terrain = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png
 
 $.getJSON('https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%201/Assignment%201/data/sf_crime.geojson', function(data) {
   var coordinatesOnly = data.features.map(function(feature) {
-    return [feature.geometry.coordinates[1], feature.geometry.coordinates[0], 1];
+    return [features.geometry.coordinates[1], features.geometry.coordinates[0], 1];
   });
 
   var heat = L.heatLayer(coordinatesOnly).addTo(map);
